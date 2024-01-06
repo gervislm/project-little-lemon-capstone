bbe0f8f2957aecb0f40b1c85c722f6b724c9b9a9
gervislm <gervislm@icloud.com>
2024-01-06 13:28:32 -0300

    nav creation and styles

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import { Footer } from "./components/Footer";
 function App() {
   return (
     <>
-      <Header />
       <Navbar />
+      <Header />
       <Main />
       <Footer />
     </>
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import logo from "../../images/Logo.svg";
+import { findAllByDisplayValue } from "@testing-library/react";
 
 export const Navbar = () => {
-  return <nav>Navbar</nav>;
+  const [menuOpen, setMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+  return (
+    <nav className={`"navbar" ${menuOpen ? "open" : ""}`}>
+      <a>
+        <img src={logo} alt="Logo" />
+      </a>
+
+      {/* mobile navbar */}
+      <div className="menu-icon">
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+
+      {/* nav items */}
+      <ul className={`nav-links ${menuOpen ? "visible" : ""}`}>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/">About</a>
+        </li>
+        <li>
+          <a href="/">Services</a>
+        </li>
+        <li>
+          <a href="/">Menu</a>
+        </li>
+        <li>
+          <a href="/">Reservations</a>
+        </li>
+        <li>
+          <a href="/">Order Online</a>
+        </li>
+        <li>
+          <a href="/">Login</a>
+        </li>
+      </ul>
+    </nav>
+  );
 };
