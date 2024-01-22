ac0402dcac5c69a07a99a0093fdd21eb0e2412bd
gervislm <gervislm@icloud.com>
2024-01-22 17:04:51 -0300

    improvement in the implementation of routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import "./App.css";
-import { HashRouter as Router } from "react-router-dom";
 import { Main } from "./components/Main";
 import { Navbar } from "./components/Navbar";
 import { Menu } from "./components/Menu";
@@ -9,9 +8,7 @@ function App() {
   return (
     <>
       <Navbar />
-      <Router>
-        <Main />
-      </Router>
+      <Main />
       <Menu />
       <Footer />
     </>
--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,16 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import App from "./App";
 import BookingForm from "./components/BookingForm";
 import Header from "./components/Header";
+import Fetch from "./fetch";
 
 test("Renders the Header heading", () => {
   render(
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   );
   const headingElement = screen.getByText("Reserve Table");
   expect(headingElement).toBeInTheDocument();
@@ -23,10 +24,24 @@ test("Renders the Header heading", () => {
 
 test("Initialize/Update Times", () => {
   render(
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   );
+
+  test("Does not render Choose Date heading initially", () => {
+    // Render the App component within BrowserRouter
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+
+    // Check that the heading "Choose Date" is not initially present
+    const headingElementNew = screen.queryByText("Choose Date");
+    expect(headingElementNew).toBeNull();
+  });
+
   const reserveButton = screen.getByRole("button");
   fireEvent.click(reserveButton);
 
