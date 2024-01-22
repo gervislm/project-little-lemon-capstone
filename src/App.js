a6eb7d70078ee30219820a1bc9a85ae688ad6a94
gervislm <gervislm@icloud.com>
2024-01-22 15:52:41 -0300

    route correction

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { Main } from "./components/Main";
 import { Navbar } from "./components/Navbar";
 import { Menu } from "./components/Menu";
